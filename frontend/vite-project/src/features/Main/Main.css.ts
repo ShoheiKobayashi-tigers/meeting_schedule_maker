cae6177bc2979b930f55fbfe221467a2b7e90878
ShoheiKobayashi-tigers <koba.5387.tigers@gmail.com>
2026-02-27 01:44:50 +0900

    #14 UI大改修　layout.css.ts4-2に適用

--- frontend/vite-project/src/features/Main/Main.css.ts
+++
@@ -1,9 +0,0 @@
-import * as s from '../../styles/layout.css.ts';
-
-export const container = s.pageLayoutDouble;
-
-export const leftPanel = s.leftPanel;
-
-export const rightPanel = s.rightPanel;
-
-export const panelCard = s.basePanelCard;
--- frontend/vite-project/src/features/Main/Main.tsx
+++ frontend/vite-project/src/features/Main/Main.tsx
@@ -1,17 +1,18 @@
 // src/features/Main/Main.tsx
-import * as s from './Main.css.ts'; // さっき作ったスタイル
 import { ScheduleTablePanel } from './panels/ScheduleTablePanel.tsx';
 import { ApplicantListPanel } from './panels/ApplicantListPanel.tsx';
+
+import * as layout from '../../styles/layout.css';
 
 export const ScheduleScreen = () => {
     // managerを引数でもらう必要がなくなります！
     return (
-        <div className={s.container}>
-            <div className={s.leftPanel}>
+        <div className={layout.pageLayoutDouble}>
+            <div className={layout.leftPanel}>
                 {/* 各パネル内部で useAppStore() を呼ぶように修正していきます */}
                 <ScheduleTablePanel />
             </div>
-            <div className={s.rightPanel}>
+            <div className={layout.rightPanel}>
                 <ApplicantListPanel />
             </div>
         </div>
